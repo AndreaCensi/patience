e82728ffbd7f1a0be3610b8acfaa775af504a4ad
Andrea Censi <andrea@cds.caltech.edu>
2011-02-16 22:05:11 -0800

    typo

--- src/patience/resources.py
+++ src/patience/resources.py
@@ -17,7 +17,7 @@ class Resource:
 
     def update(self):
         pass
-
+        
     def current_revision(self):
         return None
 
@@ -44,4 +44,4 @@ class Resource:
         
         
         else:
-            raise Exception('Uknown install type "%s".' % install_type)
+            raise Exception('Unknown install type "%s".' % install_type)
