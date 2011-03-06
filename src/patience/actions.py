a995ffec42bea7b0997602d73913dd67ee8e7807
Andrea Censi <andrea@cds.caltech.edu>
2011-03-05 18:31:44 -0800

    fixed bug with checkout

--- src/patience/actions.py
+++ src/patience/actions.py
@@ -237,21 +237,21 @@ def status2string(r, res):
         return None
 
 
-class Status(Action):
+class Checkout(Action):
     
     def __init__(self): 
-        Action.__init__(self, parallel=True, any_order=False)
+        Action.__init__(self, parallel=False, any_order=False)
 
     def single_action_starting(self, resource): pass
 
-    def single_action_result_display(self, resource, result):
-        if not isinstance(result, Exception):
-            return status2string(resource ,result)
+    def single_action_result_display(self, resource, result): pass
     
     def single_action(self, r):
         if not r.is_downloaded():
             r.checkout()
     
+Action.actions['checkout'] = Checkout()
+
 class Status(Action):
     
     def __init__(self): 
