38dafdecbf102ede6beafd4365642789e56e81e7
Andrea Censi <andrea@cds.caltech.edu>
2013-04-04 17:31:40 -0700

    minor

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(name='patience',
       author="Andrea Censi",
       author_email="andrea@cds.caltech.edu",
       url='http://github.com/AndreaCensi/patience',
-    	version="1.0",
+    	version="1.0.2",
       description="A command line tool for managing multiple git repositories.",
       package_dir={'':'src'},
-      packages=['patience'],
+      packages=find_packages('src'),
       install_requires=['pyyaml'],
       entry_points={
          'console_scripts': [
