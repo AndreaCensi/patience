aa93df864fa0c0c87cb3defdccf4b2626ce86cca
Andrea Censi <acensi@ethz.ch>
2020-12-26 11:03:47 +0100

    fix

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup, find_packages
 
-install_requires=['pyyaml', 'SystemCmd-z6', 'PyContracts3']
+install_requires = ["pyyaml", "SystemCmd-z7", "PyContracts3"]
 
-setup(name='patience',
-      author="Andrea Censi",
-      author_email="censi@mit.edu",
-      url='http://github.com/AndreaCensi/patience',
-    	version="1.5.0",
-      description="A command line tool for managing multiple git repositories.",
-      package_dir={'':'src'},
-      packages=find_packages('src'),
-      install_requires=install_requires,
-      entry_points={
-         'console_scripts': [
-           'patience = patience.main:main',
-           'patience_search = patience.patience_search:main',
-           'pat = patience.main:main',
-           'patience2html = patience.patience2html:main'
+setup(
+    name="patience",
+    author="Andrea Censi",
+    author_email="censi@mit.edu",
+    url="http://github.com/AndreaCensi/patience",
+    version="1.5.0",
+    description="A command line tool for managing multiple git repositories.",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "patience = patience.main:main",
+            "patience_search = patience.patience_search:main",
+            "pat = patience.main:main",
+            "patience2html = patience.patience2html:main",
         ]
-      },
+    },
 )
-
-
