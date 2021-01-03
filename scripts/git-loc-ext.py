acef6d711c66b3fc3751ac4b1359a4b1b594b761
Andrea Censi <acensi@ethz.ch>
2021-01-03 20:00:30 +0100

    first

--- scripts/git-loc-ext.py
+++ scripts/git-loc-ext.py
@@ -1,19 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from rfc822 import parsedate
 from time import mktime
 from datetime import datetime
 from os import popen
-from sys import argv,stderr,stdout
+from sys import argv, stderr, stdout
 from collections import namedtuple
 import sys, os
 import yaml
-FileCommit = namedtuple('FileCommit', 'date filename adds dels')
+
+FileCommit = namedtuple("FileCommit", "date filename adds dels")
+
+
 def read_commits(repo):
     date = None
-    for x in popen('git --git-dir %r log --numstat' % repo):
-        if x.startswith('Date'):
-            date=datetime(*parsedate(x[5:])[:7])
+    for x in popen("git --git-dir %r log --numstat" % repo):
+        if x.startswith("Date"):
+            date = datetime(*parsedate(x[5:])[:7])
             # t=mktime(parsedate(x[5:]))
 
         tokens = x.split()
@@ -22,21 +25,19 @@ def read_commits(repo):
             adds = int(tokens[0])
             dels = int(tokens[1])
             filename = tokens[2]
-            yield FileCommit(date=date, filename=filename,adds=adds,dels=dels)
-        
-        
-if __name__ == '__main__':
+            yield FileCommit(date=date, filename=filename, adds=adds, dels=dels)
+
+
+if __name__ == "__main__":
     if len(argv) >= 3:
         suffix = argv[2]
     else:
         suffix = None
-    repo = os.path.join(argv[1], '.git')
+    repo = os.path.join(argv[1], ".git")
 
-        
     for x in read_commits(repo):
-        if suffix and not x.filename.endswith(suffix): continue
+        if suffix and not x.filename.endswith(suffix):
+            continue
         # print(x)
-        y = {'adds': x.adds, 'date': x.date, 'dels': x.dels, 'filename': x.filename}
+        y = {"adds": x.adds, "date": x.date, "dels": x.dels, "filename": x.filename}
         yaml.dump([y], sys.stdout)
-        
-        
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = ["pyyaml", "SystemCmd-z7", "PyContracts3"]
 setup(
     name="patience",
     author="Andrea Censi",
-    author_email="censi@mit.edu",
+    author_email="github@censi.org",
     url="http://github.com/AndreaCensi/patience",
     version="1.5.0",
     description="A command line tool for managing multiple git repositories.",
